ef0fc54ef32022069c57557fe6818281ce6a2503
Julia Bernstein <jeb476@cornell.edu>
2020-11-21 19:42:32 -0500

    first commit

---
+++ jeb476_p4/DetailVC.swift
@@ -0,0 +1,9 @@
+//
+//  DetailVC.swift
+//  jeb476_p4
+//
+//  Created by Julia Bernstein on 4/10/20.
+//  Copyright © 2020 Julia Bernstein. All rights reserved.
+//
+
+import Foundation
---
+++ jeb476_p4/SongTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SongTableViewCell.swift
+//  jeb476_p4
+//
+//  Created by Julia Bernstein on 4/10/20.
+//  Copyright © 2020 Julia Bernstein. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
